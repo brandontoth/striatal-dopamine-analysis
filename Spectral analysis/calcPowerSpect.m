4869632cd4da9936d0a01c1ab284f21b249ddc7b
Brandon Toth <40743700+brandontoth@users.noreply.github.com>
2023-01-31 23:18:33 -0500

    Update calcPowerSpect.m

--- Spectral analysis/calcPowerSpect.m
+++ Spectral analysis/calcPowerSpect.m
@@ -2,7 +2,7 @@ function powerSpec = calcPowerSpect(EEG, Fs)
 
 window = hann(Fs); % hamming window of size equivalent to the sampling frequency
 noverlap = Fs * 0.5;  % have 50% overlap between windows   
-f = 0:.5:20;          % bin at .5 Hz resolution
+f = 0:.5:15;          % bin at .5 Hz resolution
 
 % pwelch power estimate
 [pxx, ~]= pwelch(EEG, window, noverlap, f, Fs);
